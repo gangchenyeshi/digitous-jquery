3bfae0c4d33b09fefe48279c49493c43103347c9
unknown <yestsering@gmail.com>
2021-01-11 14:50:56 +0100

    1 exercise commit

---
+++ exercice1/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    //console.log("Hello for testing");
+    
+    $("#square").css("background-color", "red");
+}) 
---
+++ exercice2/js/main.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+    //console.log("Hello for testing");
+    
+    $("#square").css("width", "500px");
+    $("#square").css("height", "500px");
+}) 
