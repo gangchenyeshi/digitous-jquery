71412aaeced85ae7ac6fbc84bfe3657d989f7fe1
unknown <yestsering@gmail.com>
2021-01-11 16:52:12 +0100

    8 and 9 exercise commit

--- exercice8/js/main.js
+++ exercice8/js/main.js
@@ -1,5 +1,9 @@
 $(document).ready (function () {
+   
+    
     $(".btn").click(function (){//when I click this button
-        $("#text").css("display", "block");//it show and hide the text from HTML file by default it is hide;
+    //    $("#text").css("display", "block");//it show and hide the text from HTML file by default it is hide;
+    displaySecretText();    
     })
+    
 })
