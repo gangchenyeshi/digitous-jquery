df791405237593d8dfa21899aca5a976fd4af7e2
unknown <yestsering@gmail.com>
2021-01-11 20:55:24 +0100

    21 and 22 exercise commit

---
+++ exercice21/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready (function () {
+    $(".btn").click(function () {
+        $("#square").animate({width: '500px'})
+    })
+})
---
+++ exercice22/js/main.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready (function() {
+    $(".btn").click(function () {
+        $("#text").animate({fontSize: '50px'}, "slow");
+        //$("#text").animate("fontSize", "50px");
+    })
+})
