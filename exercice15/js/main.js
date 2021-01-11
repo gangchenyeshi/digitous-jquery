0424e691901c90aa5c60b16c67381d60e89ea4b6
unknown <yestsering@gmail.com>
2021-01-11 17:41:16 +0100

    13, 14 and 15 exercise commit

---
+++ exercice15/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready (function () {
+    //console.log("test")
+    $(".btn").click(function () {
+        $("#text").addClass("float-right")
+        //$("#text").attr("class", "float-right")
+    })
+})
+
