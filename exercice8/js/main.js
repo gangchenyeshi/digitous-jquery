fea99069b3da3936f5bbd4cf7f00bc830090cbae
unknown <yestsering@gmail.com>
2021-01-11 16:39:22 +0100

    8 exercise commit

---
+++ exercice8/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready (function () {
+    $(".btn").click(function (){//when I click this button
+        $("#text").css("display", "block");//it show and hide the text from HTML file by default it is hide;
+    })
+})
