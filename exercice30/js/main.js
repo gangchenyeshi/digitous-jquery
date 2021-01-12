ffa939e95ea76f99a2ca06a136c1ef17b1056128
unknown <yestsering@gmail.com>
2021-01-12 17:04:42 +0100

    29 to 30 exercise commit

---
+++ exercice30/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+        $("input").keyup( function () {
+            if($("input").val().length > 5) {
+                $("input").addClass("is-valid");
+                $("input").removeClass("is-invalid");//if it not remove this class it store this class in console
+            } else {
+                $("input").addClass("is-invalid");
+                $("input").removeClass("is-valid");//if it not remove this class it store this class in console
+            }
+            
+        })
+    
+})
