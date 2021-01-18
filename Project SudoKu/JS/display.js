16f2b5cf3b20fd8a080286fb48cf37af32235909
unknown <yestsering@gmail.com>
2021-01-18 14:05:09 +0100

    sudoku 2 commit

---
+++ Project SudoKu/JS/display.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    //generateSudoku();
+
+    var sudokuHtml = '<div class="container">'; // create a div in Html and class = "container"
+    
+    for(var i = 0; i < 9; i ++) {
+        sudokuHtml += '<div id="row${i}" class = "row">';// create a div in Html and class = "row"
+
+        for(var j = 0; j < 9; j++) {
+            sudokuHtml += `<div id="col${j}" class ="col-1 box">${sudoku[i][j]}</div>`; // create a div in Html and class = "col-1"and closed here
+            }   
+            sudokuHtml += '</div>';//closed div class row
+        }
+    sudokuHtml = sudokuHtml + '</div>'; // div class= container is closed
+    //sudoku += '</div>'; // div class= container is closed
+
+    $("#sudoku").html(sudokuHtml);
+});
